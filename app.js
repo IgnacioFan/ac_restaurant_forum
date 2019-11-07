778aeebd93f945dcd68dbc97028622143c69322e
Ignatius Fan <fan01856472@gmail.com>
2019-11-07 08:13:50 +0800

    user can signup and the data will store into db(without validation)

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.listen(port, function () {
   db.sequelize.sync();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const restController = require('../controllers/restController.js')
+const userController = require('../controllers/userController.js')
 const adminController = require('../controllers/adminController.js')
 
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('restaurants'))
   app.get('/restaurants', restController.getRestaurants)
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
   app.get('/admin', (req, res) => res.redirect('admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
 }
