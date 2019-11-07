a6c519c16b19cef61c73d2c28bd2fdc4e003a0a5
Ignatius Fan <fan01856472@gmail.com>
2019-11-07 09:49:19 +0800

    user can sign in and redirect to forum page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
@@ -12,6 +13,9 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
@@ -24,4 +28,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
