199e7241f93e848b459d9f261ebb6865b3ef22d8
Ignatius Fan <fan01856472@gmail.com>
2019-11-08 10:26:55 +0800

    admin can upload picture as creating a restaurant info

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
-
+app.use('/upload', express.static(__dirname + '/upload'))
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
