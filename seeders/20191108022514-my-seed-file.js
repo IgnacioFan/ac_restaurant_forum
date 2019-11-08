273bd0a5d8c405f3712fb5e38031d6b323ec652e
Ignatius Fan <fan01856472@gmail.com>
2019-11-08 11:27:15 +0800

    add 50 faker date into seed

--- seeders/20191108022514-my-seed-file.js
+++ seeders/20191108022514-my-seed-file.js
@@ -1,9 +1,10 @@
 'use strict';
 const bcrypt = require('bcrypt-nodejs')
+const faker = require('faker')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [{
+    queryInterface.bulkInsert('Users', [{
       email: 'root@example.com',
       password: bcrypt.hashSync('12345', bcrypt.genSaltSync(10), null),
       isAdmin: true,
@@ -24,10 +25,24 @@ module.exports = {
       name: 'jimmy',
       createdAt: new Date(),
       updatedAt: new Date()
-    }])
+    }]);
+
+    return queryInterface.bulkInsert('Restaurants',
+      Array.from({ length: 50 }).map(d => ({
+        name: faker.name.findName(),
+        tel: faker.phone.phoneNumber(),
+        address: faker.address.streetAddress(),
+        opening_hours: '08:00',
+        image: faker.image.imageUrl(),
+        description: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      ), {});
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Users', null, {});
+    return queryInterface.bulkDelete('Restaurants', null, {});
   }
 };
