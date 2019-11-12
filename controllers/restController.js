a4d3d47978e746f5942dac6e8609a5e995e80fcc
Ignatius Fan <fan01856472@gmail.com>
2019-11-12 16:57:19 +0800

    user can use category nav list

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,13 +4,35 @@ const Category = db.Category
 
 let restController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
+    let whereQuery = {}
+    let categoryId = ""
+    if (req.query.categoryId) {
+      // convert id from string into integer
+      categoryId = Number(req.query.categoryId)
+      whereQuery['categoryId'] = categoryId
+    }
+
+    return Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
-      console.log(data)
-      return res.render('restaurants', { restaurants: data })
+      // get all categories data
+      Category.findAll().then(categories => {
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId
+        })
+      })
+
+    })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
+      console.log(restaurant)
+      return res.render('restaurant', { restaurant: restaurant })
     })
   }
 }
