8887dc28b473e282d7649a07ce3123eeb10b7d6e
Ignatius Fan <fan01856472@gmail.com>
2019-11-12 21:27:15 +0800

    ceate pager and add comment model

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,19 +1,36 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
+
 
 let restController = {
   getRestaurants: (req, res) => {
     let whereQuery = {}
     let categoryId = ""
+    let offset = 0
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       // convert id from string into integer
       categoryId = Number(req.query.categoryId)
       whereQuery['categoryId'] = categoryId
     }
 
-    return Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
-      const data = restaurants.map(r => ({
+    return Restaurant.findAndCountAll({
+      include: Category, where: whereQuery,
+      offset: offset, limit: pageLimit
+    }).then(result => {
+      let page = Number(req.query.page) || 1
+      // the maximum of the page
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+
+      const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
@@ -22,6 +39,10 @@ let restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories: categories,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next,
           categoryId: categoryId
         })
       })
