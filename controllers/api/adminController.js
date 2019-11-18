64707dae16f6c9ab45e9467fa748d6a939289d0d
Ignatius Fan <fan01856472@gmail.com>
2019-11-18 14:25:03 +0800

    api design for individual restaurants

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -1,11 +1,14 @@
-const db = require('../../models')
-const Restaurant = db.Restaurant
-const Category = db.Category
-const adminService = require('../../services/adminServices')
+const adminService = require('../../services/adminServices.js')
 
 const adminController = {
   getRestaurants: (req, res) => {
     adminService.getRestaurants(req, res, (data) => {
+      return res.json({ data })
+    })
+  },
+
+  getRestaurant: (req, res) => {
+    adminService.getRestaurant(req, res, (data) => {
       return res.json({ data })
     })
   }
